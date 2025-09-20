6f622f7982c83e9785cccf5b6da5a5054ab7bf64
wdchocopie <81132394+wdchocopie@users.noreply.github.com>
2025-09-20 12:40:37 +0700

    Create btvn1.py

---
+++ btvn1.py
@@ -0,0 +1,30 @@
+# Bài 1
+# Viết chương trình in ra thông tin học sinh trong đó có họ tên, tuổi, lớp, trường học
+# Sẽ được điểm cộng nếu chỉ sử dụng 1 hàm print()
+# mẫu của output: 
+# Họ và tên: Trần Hoàng Long
+# Tuổi: 20
+# Lớp: 12A5
+# Trường: PTLC Vinschool
+
+# Bài 2
+# Viết 1 chương trình trong đó khai báo 2 biến a, b
+# Gán giá trị cho 2 biến a, b tùy theo ý bạn với kiểu dữ liệu lần lượt là str và int
+# Sau đó in ra giá trị của 2 biến này và kiểu dữ liệu của chúng
+# Mẫu của output:
+# a = "Xin chào"
+# Kiểu dữ liệu của a là: str
+# b = 2020
+# Kiểu dữ liệu của b là: int
+# Gợi ý: Sử dụng hàm type() để in ra kiểu dữ liệu của biến
+
+# bài 3
+# Viết 1 chương trình trong đó nhận 2 giá trị nhập vào từ bàn phím trả lời các câu hỏi sau
+# Tên của bạn là gì?
+# Bạn bao nhiêu tuổi?
+# Sau đó in
+# Chào bạn <tên của bạn>, bạn đã <tuổi của bạn> tuổi rồi à?
+# output mẫu:
+# Tên của bạn là gì? Trần Hoàng Long
+# Bạn bao nhiêu tuổi? 20
+# Chào bạn Trần Hoàng Long, bạn đã 20 tuổi rồi à?
