51117d184f7894d59d29ea36e5fe29b346a3cb9d
wdchocopie <81132394+wdchocopie@users.noreply.github.com>
2025-09-20 12:52:41 +0700

    Update 20-9-2025.py

--- 20-9-2025.py
+++ 20-9-2025.py
@@ -1,3 +1,10 @@
+# python là gì? 
+# Python là một ngôn ngữ lập trình đa mục đích, dễ học và sử dụng.
+# Python được sử dụng rộng rãi trong phát triển web, khoa học dữ liệu, trí tuệ nhân tạo, tự động hóa và nhiều lĩnh vực khác.
+# Python có cú pháp đơn giản, dễ đọc và viết, giúp lập trình viên tập trung
+# vào giải quyết vấn đề thay vì lo lắng về cú pháp phức tạp.
+# Python có một cộng đồng lớn và nhiều thư viện hỗ trợ, giúp việc phát triển ứng dụng nhanh chóng và hiệu quả.
+
 # Hàm print:
 # sử dụng để in ra màn hình
 print("Hello, World!")
@@ -22,4 +29,5 @@ age = input("Enter your age: ")
 age = int(age)  # chuyển đổi chuỗi nhập vào thành số nguyên
 next_year_age = age + 1
 print("Next year, you will be", next_year_age, "years old.")
+
 
