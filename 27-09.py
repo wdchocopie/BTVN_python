1fb45f7f8cf29ef70c463ea96dc810dca7b6773e
wdchocopie <81132394+wdchocopie@users.noreply.github.com>
2025-09-27 13:03:50 +0700

    Add files via upload

---
+++ 27-09.py
@@ -0,0 +1,17 @@
+# toán tử
+# + - * / // % **
+# điều kiện
+# True 1 có điện False 0 không có điện
+# == != < <= > >=
+
+# nhập 2 số bất kì
+# kiểm tra a == b, a != b
+# in ra kq
+# a bằng b
+# a không bằng b
+a = int(input("a = "))
+b = int(input("b = "))
+if a == b:
+    print("a = b")
+else:
+    print("a != b")
---
+++ btvn2.py
@@ -0,0 +1,22 @@
+# Bài 4
+# Viết 1 chương trình hiển thị thông tin học sinh với menu như sau:
+# 1. Hiển thị thông tin học sinh
+# 2. Hiển thị lời phê bình của giáo viên
+# Nếu người dùng chọn 1 thì hiển thị thông tin học sinh
+# Nếu người dùng chọn 2 thì hiển thị lời phê bình của giáo viên
+# Nếu người dùng chọn các số khác thì in ra "Lựa chọn không hợp lệ"
+# output mẫu 1:
+# Chọn chức năng bạn muốn sử dụng:
+# 1. Hiển thị thông tin học sinh
+# 2. Hiển thị lời phê bình của giáo viên
+# Lựa chọn của bạn: 1
+# Họ và tên: Trần Hoàng Long
+# Tuổi: 20
+# Lớp: 12A5
+# Trường: PTLC Vinschool
+# output mẫu 2:
+# Chọn chức năng bạn muốn sử dụng:
+# 1. Hiển thị thông tin học sinh
+# 2. Hiển thị lời phê bình của giáo viên
+# Lựa chọn của bạn: 2
+# Em là học sinh ngoan, chăm chỉ, lễ phép, biết vâng lời thầy cô và ông bà cha mẹ
