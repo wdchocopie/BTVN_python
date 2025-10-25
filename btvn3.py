e9ed570a5a4b204b1e1c0edcd8960c579d7f79cc
wdchocopie <81132394+wdchocopie@users.noreply.github.com>
2025-10-25 16:08:33 +0700

    Create btvn3.py

---
+++ btvn3.py
@@ -0,0 +1,10 @@
+# bài 1: viết 1 chương trình in ra 1 chuỗi (String) nhập từ bàn phím, lặp lại liên tục. số lần lặp được nhập từ bàn phím
+# Ouput:
+# Chuỗi: Python
+# số lần lặp: 5
+# Python
+# Python
+# Python
+# Python
+# Python
+
