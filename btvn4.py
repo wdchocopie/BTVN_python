2e0655ada56ecc13075dbc00aa399b3408d6d420
wdchocopie <81132394+wdchocopie@users.noreply.github.com>
2025-11-08 12:42:44 +0700

    Create btvn4.py

---
+++ btvn4.py
@@ -0,0 +1,7 @@
+# Viết chương trình Python cho phép người dùng nhập vào hai số (giới hạn dưới và giới hạn trên)
+# Sau đó tính tổng tất cả các số nguyên nằm trong khoảng từ giới hạn dưới đến giới hạn trên (bao gồm cả hai đầu).
+# Ví dụ mẫu output:
+
+# Nhập số bắt đầu: 3
+# Nhập số kết thúc: 7
+# Tổng các số từ 3 đến 7 là: 25
