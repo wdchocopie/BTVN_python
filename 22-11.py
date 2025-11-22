e95f34be05137216cde7d3b8d2d84140cbaffed5
wdchocopie <81132394+wdchocopie@users.noreply.github.com>
2025-11-22 15:55:32 +0700

    Add files via upload

---
+++ 22-11.py
@@ -0,0 +1,13 @@
+# while [điều kiện]:    
+# điều kiện -> đúng hay sai -> True / False
+a = 2
+while a > 0:
+    print()
+    a = a -1
+while 1 == 1:
+    print()
+while True:
+    print()
+
+# 1 vòng lặp vô tận in ra tên của các bạn
+# 5 lần, sử dụng while
